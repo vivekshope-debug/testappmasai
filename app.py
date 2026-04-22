7fb35435d4da7f7299c9b3d98668a3f1b0bff1ae
vivekshope-debug <vivek.shope@gmail.com>
2026-04-22 23:45:26 +0100

    Initialize Streamlit app with basic functionality

---
+++ app.py
@@ -0,0 +1,16 @@
+app.py
+
+import streamlit as st
+
+st.set_page_config(page_title="Test App", layout="Centered")
+
+st.title ("Streamlist App is working")
+
+st.write("If you can see this, your deployment is successful.")
+
+name = st.text_inout("Enter your name:")
+
+if name:
+  st. success(f"Hello {name}, your streamli app ia live")
+
+st.button ("click me")
